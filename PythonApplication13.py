296d7059fb1ba0c7ae6fd8cc09ee15657880dded
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-02 16:02:53 +0300

    Add files via upload

---
+++ PythonApplication13.py
@@ -0,0 +1,16 @@
+t = int(input())
+n = int(input())
+weights = []
+for i in range(n):
+    weights.append(int(input()))
+    weights.sort()
+    i = 0
+    j = n-1
+    boats = 0 
+    while i <=j:
+        if weights[j] + weights[i] <= t:
+            i +=1
+            j -=1
+            boats +=1
+
+        
