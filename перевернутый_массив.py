6b12222b37c13905cc3c8e235c9ffeada8bfeb12
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-22 20:53:33 +0300

    Add files via upload

---
+++ перевернутый_массив.py
@@ -0,0 +1,9 @@
+﻿n = int(input("Введите количество чисел:"))
+numbers = []
+for _ in range (n):
+num  = int(input())
+numbers.append(num )
+reversed_numbers = list (reversed(numbers))
+print("Перевёрнутый массив:")
+for num  in reversed_numbers:
+    print(num )
