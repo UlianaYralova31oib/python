49c0124b3805b7e7e59aed3e352c9923447465b8
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-22 21:32:15 +0300

    Add files via upload

---
+++ Встречалось_ли_ранее.py
@@ -0,0 +1,8 @@
+﻿numbers = list(map(int,input().split()))
+seen_numbers = set()
+for number in numbers:
+    if number in seen_numbers:
+        print("YES")
+    else:
+        print("NO")
+        seen_numbers.add(number) 
