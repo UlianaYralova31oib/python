861df30bc3c9ecac8a609fbd7ad1e92a2fee1106
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-24 20:16:48 +0300

    Add files via upload

---
+++ Объект_Aytobus_1.py
@@ -0,0 +1,11 @@
+﻿class Transport:
+    def _init_(self, name, speed, mileage):
+        self.name = name
+        self.speed = speed
+        self.mileage = mileage
+
+class Autobus(Transport):
+    pass
+
+autobus = Autobus("Renaul Logan", 180, 12)
+print(f"Название автомобиля: {autobus.name} Скорость: {autobus.speed} Пробег: {autobus.mileage}")
