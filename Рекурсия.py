34d289e29eddd788b1279ee624fa8d2863364b70
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-24 19:21:16 +0300

    Add files via upload

---
+++ Рекурсия.py
@@ -0,0 +1,9 @@
+﻿def print_list (lst, index):
+    if index < len (lst):
+        print (lst [index])
+        print_list(lst,index+1)
+    else:
+        print("Конец списка")
+        my_list=[0 ,1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12, 13 ,14 ,15 ,16]
+        print_list(my_list,0)
+        
