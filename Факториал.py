7ace7149729e34319b0618439219f0ab0f6509e1
UlianaYralova31oib <146767747+UlianaYralova31oib@users.noreply.github.com>
2023-11-24 19:38:28 +0300

    Add files via upload

---
+++ Факториал.py
@@ -0,0 +1,15 @@
+﻿def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+        return result
+
+def factorials_list(n):
+    fact = factorial(n)
+    result_list = [factorial(x) for x in 
+                   range(fact, 0, -1)]
+    return result_list
+
+input_number = 3
+print("Факториал числа", input_number, ":", factorial(input_number))
+print("Список факториалов в убывающем порядке:", factorials_list(input_number))
